8f217c55e7e46c34c1273f08d93e5bec7d4dba0e
Jennifer Levy <jenniferlevy@Jennifers-MBP.home>
2019-05-10 15:56:05 -0400

    add error message for non letter keys

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ function init(){
 		if (event.keyCode >= 65 && event.keyCode <= 90){
 			var letter = String.fromCharCode(event.keyCode);
 			console.log(letter);
+		} else {
+			console.log("that's not a letter!")
 		}
 
 		
