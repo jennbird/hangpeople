dbf52e3c5545f2f9418ca2eb309293f610debfee
Jennifer Levy <jenniferlevy@Jennifers-MBP.home>
2019-05-10 15:50:27 -0400

    add ability to recognize pressed letters a to z

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello world!");
+init()
+
+function init(){
+
+	console.log("hey");
+
+
+	window.addEventListener("keyup", function(event){
+
+		if (event.keyCode >= 65 && event.keyCode <= 90){
+			var letter = String.fromCharCode(event.keyCode);
+			console.log(letter);
+		}
+
+		
+	})
+}	
